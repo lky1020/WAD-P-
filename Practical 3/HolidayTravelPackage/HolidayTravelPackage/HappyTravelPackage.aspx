0836c341f38045f342aaef565b59a198d1c29c9a
lky1020 <57024183+lky1020@users.noreply.github.com>
2021-01-30 19:48:07 +0800

    Upload P3 - HappyTravelPackage

---
+++ Practical 3/HolidayTravelPackage/HolidayTravelPackage/HappyTravelPackage.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HappyTravelPackage.aspx.cs" Inherits="HolidayTravelPackage.HappyTravelPackage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <span style="font-size: 36px; font-weight: bold; text-decoration:underline;">Holidy Travel Package</span>
+
+            <br /><br />
+
+            <span style="font-size: 32px"><asp:Label ID="lblMsg" runat="server"></asp:Label></span>
+
+            <br /><br />
+
+            Your Budget: <asp:TextBox ID="txtBudget" runat="server"></asp:TextBox>
+
+            <br /><br />
+
+            Choose your destination:<br /><br />
+
+            <table style="width: 25%">
+                <tr>
+                    <td>From</td>
+                    <td>To</td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <asp:DropDownList ID="ddlFrom" runat="server">
+                            <asp:ListItem>KL</asp:ListItem>
+                            <asp:ListItem>Penang</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                    <td>
+                        <asp:DropDownList ID="ddlTo" runat="server">
+                            <asp:ListItem>Japan</asp:ListItem>
+                            <asp:ListItem>Auckland</asp:ListItem>
+                        </asp:DropDownList>
+                    </td>
+                </tr>
+            </table>
+
+            <br /><br />
+
+            <asp:Button ID="btnCheck" runat="server" Text="Check" />
+
+            <br /><br />
+
+            <asp:Label ID="lblDestination" runat="server"></asp:Label><br />
+            <asp:Label ID="lblMessage" runat="server"></asp:Label><br />
+            <asp:Label ID="lblMoneyLeft" runat="server"></asp:Label><br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Practical 3/HolidayTravelPackage/HolidayTravelPackage/HappyTravelPackage.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HolidayTravelPackage
+{
+    public partial class HappyTravelPackage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(IsPostBack == false)
+            {
+                lblMsg.Text = "Thank you for using our service";
+            }
+            else
+            {
+                lblMsg.Text = "Happy Holiday from MOM Travel";
+
+                lblDestination.Text = "Your Destination : " + ddlFrom.SelectedValue.ToString() + "-" + ddlTo.SelectedValue.ToString();
+
+                int differentPrice = differencesPrice();
+
+                if (differentPrice < 0)
+                {
+                    lblMessage.Text = "Sorry! Can't go for holiday!";
+                    lblMoneyLeft.Text = "";
+                }
+                else
+                {
+                    lblMessage.Text = "Yay! Can go for holiday!";
+                    lblMoneyLeft.Text = "RM " + differentPrice + " extra pocket money!";
+                }
+
+            }
+            
+        }
+
+        private int calculatePrice()
+        {
+            String from = ddlFrom.SelectedValue.ToString();
+            String to = ddlTo.SelectedValue.ToString();
+
+            if(from == "KL" && to == "Japan")
+            {
+                return 2400;
+            }
+            else if (from == "KL" && to == "Auckland")
+            {
+                return 2200;
+            }
+            else if (from == "Penang" && to == "Japan")
+            {
+                return 3400;
+            }
+            else
+            {
+                return 3200;
+            }
+        }
+
+        private int differencesPrice()
+        {
+            //get budget
+            int budget = Int32.Parse(txtBudget.Text);
+
+            return budget - calculatePrice();
+        }
+    }
+}
